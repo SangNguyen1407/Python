d3585a9dc10f8f381c5dc190a85f45236d955db4
SangNguyen1407 <14.sangnguyen.07@gmail.com>
2023-09-17 16:17:59 +0900

    update file

--- file_transfer/read_write_file.py
+++ file_transfer/read_write_file.py
@@ -6,12 +6,20 @@ import tempfile
 
 def rw_text():
     # write data into file
-    # write file with 'w'
+    # write file with -> 'w'
     with open('mydata.txt', 'w', encoding='utf-8') as wf:
         wf.write('text1\n')
-        wf.write('text2')
+        wf.write('text2\n')
 
-    # read file with 'r'
+    # append data into file -> 'a'
+    with open('mydata.txt', 'a', encoding='utf-8') as wf:
+        print( 'Hello World 1!', file=wf )
+
+    # append data into file -> 'wt'
+    with open('mydata.txt', 'wt') as wf:
+        wf.write( 'Hello World 2!' )
+
+    # read file with -> 'r'
     with open('mydata.txt', 'r', encoding='utf-8') as rf:
         text = rf.read()    
         print(text)
@@ -20,5 +28,7 @@ def rw_text():
     with NamedTemporaryFile() as tempfile:
         print('dirname is: ', tempfile.name)
     
+    print('\n')
+
 if __name__ == '__main__':
     rw_text()
